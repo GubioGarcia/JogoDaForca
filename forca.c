8622d25c24e5098845ae0ec1ead2b3037ad36b04
Gubio Garcia <gubiogarcia@gmail.com>
2023-07-20 17:40:11 -0300

    update

--- forca.c
+++ forca.c
@@ -7,18 +7,39 @@ int main (){
     sprintf(palavraSecreta, "MELANCIA");
 
     int acertou = 0;
-    int enforcou =  1;
+    int enforcou = 0;
+
+    char vetorPalpites[26];
+    int tentativas = 0;
+
+    printf("*   JOGO DA FORCA   *\n\n");
 
     do {
-        char palpite;
-        scanf("%c", &palpite);
+        for (int i = 0; i < strlen(palavraSecreta); i++) {
+            int achou = 0;
 
-        for(int i = 0; i < strlen(palavraSecreta); i++){
-            if (palavraSecreta[i] == palpite){
-                printf("A posicao %d tem essa letra!\n", i);
+            for (int j = 0; j < tentativas; j++) {
+                if (vetorPalpites[j] == palavraSecreta[i]) {
+                    achou = 1;
+                    break;
+                }
+            }
+            
+            if (achou) {
+                printf ("%c ", palavraSecreta[i]);
+            } else {
+                printf ("_ ");
             }
         }
 
+        printf ("\n");      
+
+        char palpite;
+        printf ("Qual letra? ");
+        scanf (" %c", &palpite);
+
+        vetorPalpites[tentativas] = palpite;
+        tentativas++;
     } while (!acertou && !enforcou);
     
 }
